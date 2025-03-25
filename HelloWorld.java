1fcf6aafc392971c7d966c9fd3dd48c70ccc6abe
CrisMtz155 <crisjmtz155@gmail.com>
2025-03-24 23:10:49 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+    public static void main(String[] arg){
+       //The main() method is the first line and starting point and is needed to for the JVM to start its program.
+       System.out.println("Hello, World!");
+       //Used System.out.println to have output the words Hello, World! on a line by itself.
+       //System.out.println is used when we want to output or print words and sentences and other things.
+       System.out.println("My name is Cristian Martinez.");
+       //I Used system.out.println to start a new line and introduce myself.
+       System.out.println("My favorite food is a torta.");
+       // I started a new line and said my favorite food.
+       System.out.println("I would love to meet Frank Ocean who is a music artist and beg him to drop more music.");
+       // I Started a new line and answered which celebrity that I would like to meet rn.
+       System.out.println("My least favorite chore at home is washing dishes.");
+       // I started a new line and said my least favorite chore.
+       System.out.println("I rather be in the gym or playing video games instead of my lab assignments right now.");
+       //I started a new line and answered what I would rather be doing right now instead of my lab assignments.
+   } //Comments are used to help leave notes that we see to help explain our code.
+}
+    
